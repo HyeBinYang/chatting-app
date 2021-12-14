9201e0254906765a8e771c20fc65f482e36506b6
HyeBinYang <skdisk7368@gmail.com>
2021-12-15 08:06:46 +0900

    로그인 폼 state 추가

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../authForm.scss";
 import SearchLinks from "./SearchLinks";
 import { useNavigate } from "react-router-dom";
 
+interface LoginFormState {
+  username: string;
+  password: string;
+}
+
 function LoginForm() {
   const navigate = useNavigate();
+  const [loginForm, setLoginForm] = useState<LoginFormState>({
+    username: "",
+    password: "",
+  });
 
   const links = [
     {
@@ -21,7 +30,12 @@ function LoginForm() {
     },
   ];
 
-  const login = () => {
+  const onChangeLoginForm = (e: any) => {
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+  };
+
+  const login = (e: any) => {
+    e.preventDefault();
     navigate("/users");
   };
 
@@ -31,8 +45,22 @@ function LoginForm() {
         <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="logo" />
       </div>
       <form className="authForm__form" onSubmit={login}>
-        <input className="form__input" type="text" placeholder="아이디" />
-        <input className="form__input" type="password" placeholder="비밀번호" />
+        <input
+          onChange={onChangeLoginForm}
+          className="form__input"
+          type="text"
+          value={loginForm.username}
+          name="username"
+          placeholder="아이디"
+        />
+        <input
+          onChange={onChangeLoginForm}
+          className="form__input"
+          type="password"
+          value={loginForm.password}
+          name="password"
+          placeholder="비밀번호"
+        />
         <button className="form__btn active" type="submit">
           로그인
         </button>
