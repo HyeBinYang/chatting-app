7ccab12eb26901e0b4f0cdd50958c5ffdc86159a
HyeBinYang <skdisk7368@gmail.com>
2021-12-18 23:49:02 +0900

    파이어베이스 store 추가

--- client/src/server/firebase.ts
+++ client/src/server/firebase.ts
@@ -1,8 +1,12 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import "firebase/compat/firestore";
 import { firebaseConfig } from "../config/firebase";
 
 // firebaseConfig 정보로 firebase 시작
 firebase.initializeApp(firebaseConfig);
 
+console.log(firebase.firestore());
+
 export const auth = firebase.auth;
+export const store = firebase.firestore();
