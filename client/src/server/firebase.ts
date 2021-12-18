85369b4d536067c6bcaad13c545a73d6ced51d36
HyeBinYang <skdisk7368@gmail.com>
2021-12-18 16:59:55 +0900

    유저정보 database 테이블 생성 완료

--- client/src/server/firebase.ts
+++ client/src/server/firebase.ts
@@ -1,8 +1,10 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import "firebase/compat/database";
 import { firebaseConfig } from "../config/firebase";
 
 // firebaseConfig 정보로 firebase 시작
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth;
+export const database = firebase.database();
