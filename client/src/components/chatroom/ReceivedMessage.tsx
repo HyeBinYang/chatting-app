d8461ad28edf3564cb10cef83cad66d3d52b9c4d
HyeBinYang <skdisk7368@gmail.com>
2021-12-24 20:54:06 +0900

    채팅방 상대유저이름 보이기 구현

--- client/src/components/chatroom/ReceivedMessage.tsx
+++ client/src/components/chatroom/ReceivedMessage.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function ReceivedMessage() {
+  const location = useLocation();
+
   return (
     <div className="ReceivedMessage">
       <div className="ReceivedMessage__photo">
         <img src="https://picsum.photos/200" alt="" />
       </div>
       <div className="ReceivedMessage__info">
-        <div className="info__username">username</div>
+        <div className="info__username">{location.state.to}</div>
         <div className="info__message">
           <span>
             메세지 내용메세지 내용메세지 내용메세지 내용메세지 내용메세지 내용메세지 내용메세지 내용메세지 내용
