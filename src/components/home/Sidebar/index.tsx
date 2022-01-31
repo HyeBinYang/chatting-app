304e229d78ca58df333140fc6df89ed7521b5aa6
HyeBinYang <skdisk7368@gmail.com>
2022-01-31 22:36:46 +0900

    사이드바 클릭할 때 색 변화 구현

--- src/components/home/Sidebar/index.tsx
+++ src/components/home/Sidebar/index.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { FaUser } from "react-icons/fa";
 import { BsFillChatFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <div className="sidebar">
-      <Link to="/">
-        <FaUser />
-      </Link>
-      <Link to="/rooms">
-        <BsFillChatFill />
-      </Link>
+      {currentPath === "/" ? (
+        <Link to="/">
+          <FaUser className="active" />
+        </Link>
+      ) : (
+        <Link to="/">
+          <FaUser />
+        </Link>
+      )}
+
+      {currentPath === "/rooms" ? (
+        <Link to="/rooms">
+          <BsFillChatFill className="active" />
+        </Link>
+      ) : (
+        <Link to="/rooms">
+          <BsFillChatFill />
+        </Link>
+      )}
     </div>
   );
 };
