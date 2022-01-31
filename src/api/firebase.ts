74e1662f682d9be0bd1b3278345d104c0ce50857
HyeBinYang <skdisk7368@gmail.com>
2022-01-31 23:03:11 +0900

    회원가입 구현

---
+++ src/api/firebase.ts
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config/firebase";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getFirestore(app);
--- src/components/auth/signup/SignupForm.tsx
+++ src/components/auth/signup/SignupForm.tsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, database } from "../../../api/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+interface ISignupForm {
+  email: string;
+  name: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 const SignupForm = () => {
+  const [signupForm, setSignupForm] = useState<ISignupForm>({
+    email: "",
+    name: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const onChangeSignupForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignupForm({ ...signupForm, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitSignupForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const { email, name, password } = signupForm;
+      // 1. 파이어베이스에게 회원가입 요청
+      const newUser = await createUserWithEmailAndPassword(auth, email, password);
+      // 2. firestore에 기타 data 저장
+      await setDoc(doc(database, "Users", newUser.user.uid), { email, name });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="auth-form">
-      <input type="text" placeholder="이메일" />
-      <input type="text" placeholder="이름" />
-      <input type="password" placeholder="비밀번호" />
-      <input type="password" placeholder="비밀번호 재입력" />
+    <form className="auth-form" onSubmit={onSubmitSignupForm}>
+      <input onChange={onChangeSignupForm} type="text" placeholder="이메일" name="email" value={signupForm.email} />
+      <input onChange={onChangeSignupForm} type="text" placeholder="이름" name="name" value={signupForm.name} />
+      <input
+        onChange={onChangeSignupForm}
+        type="password"
+        placeholder="비밀번호"
+        name="password"
+        value={signupForm.password}
+      />
+      <input
+        onChange={onChangeSignupForm}
+        type="password"
+        placeholder="비밀번호 재입력"
+        name="passwordConfirm"
+        value={signupForm.passwordConfirm}
+      />
       <button>회원가입</button>
     </form>
   );
