1455ba9ea75b84c62e14002bba285c11ba89b355
HyeBinYang <skdisk7368@gmail.com>
2022-01-31 11:25:51 +0900

    Home page 컴포넌트 구조화 안료

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Main from "./pages/Main";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/main" element={<Main />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
