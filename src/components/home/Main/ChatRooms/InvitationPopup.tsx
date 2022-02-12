ee9f66380e9f1f4be1e9a727d0f7c4e9fc217ecd
HyeBinYang <skdisk7368@gmail.com>
2022-02-12 23:53:22 +0900

    초대 팝업 로딩 구현

--- src/components/home/Main/ChatRooms/InvitationPopup.tsx
+++ src/components/home/Main/ChatRooms/InvitationPopup.tsx
@@ -1,55 +1,80 @@
-import React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { auth, database } from "../../../../api/firebase";
+import { PopupContext } from "../../../../store/PopupStore";
+import { UserContext } from "../../../../store/UserStore";
 
 const InvitationPopup = () => {
+  const popupContext = useContext(PopupContext);
+  const userContext = useContext(UserContext);
+  const [loading, setLoading] = useState(true);
+
+  const getUserName = async (uid: string) => {
+    const docSnap = await getDoc(doc(database, "Users", uid));
+
+    if (docSnap.exists()) {
+      userContext.setName(docSnap.data().name);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        userContext.setUid(user.uid);
+        await getUserName(user.uid);
+
+        const q = query(collection(database, "Users"), where("id", "!=", user.uid));
+        const snapShot = await getDocs(q);
+
+        snapShot.forEach((doc) => {
+          const friend = {
+            id: doc.data().id,
+            email: doc.data().email,
+            name: doc.data().name,
+          };
+
+          userContext.setFriends([...userContext.friends, friend]);
+        });
+
+        setLoading(false);
+      }
+    });
+
+    return () => {
+      unsubscribeAuth();
+      userContext.setFriends([]);
+    };
+  }, []);
+
   return (
-    <div className="popup">
-      <div className="popup__search">
-        <h3>대화상대 선택</h3>
-        <input className="textbox" type="text" placeholder="이름으로 검색" />
-      </div>
-      <ul className="popup__userlist">
-        <li className="user">
-          <div className="user__info">
-            <img src="https://picsum.photos/200" alt="" />
-            <span className="user__name">로빈</span>
+    <>
+      {loading ? null : (
+        <div className="popup">
+          <div className="popup__search">
+            <h3>대화상대 선택</h3>
+            <input className="textbox" type="text" placeholder="이름으로 검색" />
           </div>
-          <div className="user__checkbox">
-            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
+          <ul className="popup__userlist">
+            {userContext.friends.map((friend) => (
+              <li className="user" key={friend.id}>
+                <div className="user__info">
+                  <img src="https://picsum.photos/200" alt="" />
+                  <span className="user__name">{friend.name}</span>
+                </div>
+                <div className="user__checkbox">
+                  <input className="checkbox" type="checkbox" />
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="popup__buttons">
+            <button onClick={() => popupContext.setInvitationPopup(false)}>취소</button>
+            <button>확인</button>
           </div>
-        </li>
-        <li className="user">
-          <div className="user__info">
-            <img src="https://picsum.photos/200" alt="" />
-            <span className="user__name">로빈</span>
-          </div>
-          <div className="user__checkbox">
-            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
-          </div>
-        </li>
-        <li className="user">
-          <div className="user__info">
-            <img src="https://picsum.photos/200" alt="" />
-            <span className="user__name">로빈</span>
-          </div>
-          <div className="user__checkbox">
-            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
-          </div>
-        </li>
-        <li className="user">
-          <div className="user__info">
-            <img src="https://picsum.photos/200" alt="" />
-            <span className="user__name">로빈</span>
-          </div>
-          <div className="user__checkbox">
-            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
-          </div>
-        </li>
-      </ul>
-      <div className="popup__buttons">
-        <button>취소</button>
-        <button>확인</button>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
