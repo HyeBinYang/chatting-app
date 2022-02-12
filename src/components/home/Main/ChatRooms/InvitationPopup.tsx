4f8be25918e0c5e1a493787edd65efe05687ffb4
HyeBinYang <skdisk7368@gmail.com>
2022-02-12 23:24:18 +0900

    초대 팝업 구현

--- src/components/home/Main/ChatRooms/InvitationPopup.tsx
+++ src/components/home/Main/ChatRooms/InvitationPopup.tsx
@@ -4,20 +4,51 @@ const InvitationPopup = () => {
   return (
     <div className="popup">
       <div className="popup__search">
-        <input type="text" placeholder="친구 검색" />
+        <h3>대화상대 선택</h3>
+        <input className="textbox" type="text" placeholder="이름으로 검색" />
       </div>
       <ul className="popup__userlist">
         <li className="user">
           <div className="user__info">
-            <img src="" alt="" />
+            <img src="https://picsum.photos/200" alt="" />
             <span className="user__name">로빈</span>
           </div>
           <div className="user__checkbox">
             <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
-            {/* <label htmlFor="user"></label> */}
+          </div>
+        </li>
+        <li className="user">
+          <div className="user__info">
+            <img src="https://picsum.photos/200" alt="" />
+            <span className="user__name">로빈</span>
+          </div>
+          <div className="user__checkbox">
+            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
+          </div>
+        </li>
+        <li className="user">
+          <div className="user__info">
+            <img src="https://picsum.photos/200" alt="" />
+            <span className="user__name">로빈</span>
+          </div>
+          <div className="user__checkbox">
+            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
+          </div>
+        </li>
+        <li className="user">
+          <div className="user__info">
+            <img src="https://picsum.photos/200" alt="" />
+            <span className="user__name">로빈</span>
+          </div>
+          <div className="user__checkbox">
+            <input onChange={() => console.log("sda")} className="checkbox" type="checkbox" id="user" />
           </div>
         </li>
       </ul>
+      <div className="popup__buttons">
+        <button>취소</button>
+        <button>확인</button>
+      </div>
     </div>
   );
 };
